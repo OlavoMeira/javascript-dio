3e49d105088086bcb78e46effa51804a82eb8c9c
OlavoMeira <olavomeiradeveloper@gmail.com>
2026-02-06 02:25:01 -0300

    importacao e exportacao

--- modulo-001/00-lista-de-exercicio/04-listas-e-estruturas-de-repeticao/exe01.js
+++ modulo-001/00-lista-de-exercicio/04-listas-e-estruturas-de-repeticao/exe01.js
@@ -1,4 +1,6 @@
 /* 1) Crie um programa que dado um número imprima a tabuada*/
+
+
 function tabuada (index){
 
 for (let i = 1; i <= 10; i++) {
---
+++ modulo-001/00-lista-de-exercicio/05-importacao-exportacao/main.js
@@ -0,0 +1,25 @@
+/*  1) Faça um programa que receba um número e escreva a sua respectiva tabuada.
+        Exemplo:
+            Entrada:
+                2
+
+            Saída:
+                2x1 = 2
+                2x2 = 4
+                2x3 = 6
+                2x4 = 8
+                2x5 = 10
+                2x6 = 12
+                2x7 = 14
+                2x8 = 16
+                2x9 = 18
+                2x10 = 20
+ */
+
+/*  Importa,ão da primeira atividade arquivo exe01-auxiliar.js  funcionando coretamente   descomente para testar.
+const { gets, print } = require('./exe01-auxiliar');
+
+print(2);
+
+*/
+
